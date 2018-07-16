ae054158139265a41e2ae823eb009090354d3da7
Eternaldeath <390353137@qq.com>
2018-07-16 09:09:45 +0800

    作业错误修改

--- 第三十一天到第四十一天 精明的小卖家/line.js
+++ 第三十一天到第四十一天 精明的小卖家/line.js
@@ -1,6 +1,7 @@
 function drawLinePic(temp){
 	var canvasLine = document.querySelector('#canvasLine');
 	var ctx_line = canvasLine.getContext('2d');
+	var ctx_line2 = canvasLine.getContext('2d');
 	var data_area = [];
 	data_area = data_area.concat(temp);
 	// 设置数据点间隔
@@ -21,13 +22,12 @@ function drawLinePic(temp){
 	
 	// 绘制数据点
 	for(var i=0;i<data_area.length;i++){	
-		ctx_line.beginPath();
-		ctx_line.strokeStyle = '#ffff'+j;
-		ctx_line.moveTo(gap,140-data_area[i]*0.2);			
-		ctx_line.lineTo(gap+22,140-data_area[i+1]*0.2);
-		ctx_line.arc(gap,140-data_area[i]*0.2,2,0,Math.PI*2);
+		ctx_line2.beginPath();
+		ctx_line2.moveTo(gap,140-data_area[i]*0.2);			
+		ctx_line2.lineTo(gap+22,140-data_area[i+1]*0.2);
+		ctx_line2.arc(gap,140-data_area[i]*0.2,2,0,Math.PI*2);
 		gap += 22;
-		ctx_line.stroke();
+		ctx_line2.stroke();
 	}
 	
 
