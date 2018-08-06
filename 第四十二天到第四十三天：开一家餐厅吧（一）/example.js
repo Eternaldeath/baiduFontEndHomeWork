4a3648b3974f5d5714e74398cfce2a6f5da734ef
Eternaldeath <390353137@qq.com>
2018-08-06 15:43:56 +0800

    完成作业

--- 第四十二天到第四十三天：开一家餐厅吧（一）/example.js
+++ 第四十二天到第四十三天：开一家餐厅吧（一）/example.js
@@ -1,29 +1,82 @@
-function Restaurant(money,set,staff,name){
-	this.money = money;
-	this.set = set;
+// 餐厅类
+function restaurant(cash,seats,staff){
+	this.cash = cash;
+	this.seats = seats;
 	this.staff = [];
+}
 
-	this.hire = function(name){
-		this.staff.push(name);
-	}
+restaurant.prototype.hire = function(people){
+	this.staff.push(people.name);
+}
 
-	this.fire = function(name){
-		this.staff.pop(name);
+restaurant.prototype.fire = function(people){
+	var staffName = this.staff.indexOf(people.name);
+	if(staffName > -1){
+		this.staff.splice(staffName,1);
 	}
 }
 
-function staffs(ID,name,cash){
+//职员类
+function memeber(ID,name,salary){
 	this.ID = ID;
 	this.name = name;
-	this.cash = cash;
-	let work = 0;
+	this.salary = salary;
+}
 
-	this.finishWork = function(){
-		work++;
+memeber.prototype.work = function(){
+	alert("我工作了一次")
+}
+
+function waiter(ID,name,salary){
+	memeber.call(this,ID,name,salary);
+}
+	//服务员
+waiter.prototype.waiterWork = function(par){
+	if(par.isArray()){
+		alert(par);
+	}else{
+		alert("上菜");
 	}
 }
-
-function writer(ID,name,cash){
-	this.staffs = staffs;
-	this.staffs(ID,name,cash);
+	//厨师
+function Cook(ID,name,salary){
+	memeber.call(this,ID,name,salary);
 }
+
+Cook.prototype.cookWork = function(){
+	alert("做菜");
+}
+
+//顾客
+function customer(){
+
+}
+
+customer.prototype.order = function(){
+	alert("点菜");
+}
+
+customer.prototype.eat = function(){
+	alert("吃菜");
+}
+
+//菜品
+function food(foodName,cost,price){
+	this.foodName = foodName;
+	this.cost = cost;
+	this.price = price;
+}
+
+//测试用例
+var ifeRestaurant = new restaurant({
+    cash: 1000000,
+    seats: 20,
+    staff: []
+});
+
+var newCook = new Cook(1,"Tony", 10000);
+ifeRestaurant.hire(newCook);
+console.log(ifeRestaurant.staff);
+
+ifeRestaurant.fire(newCook);
+console.log(ifeRestaurant.staff);
