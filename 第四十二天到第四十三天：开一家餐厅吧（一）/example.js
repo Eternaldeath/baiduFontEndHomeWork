a186dff91d5b681159c45178de61df883f090015
Eternaldeath <390353137@qq.com>
2018-08-13 15:44:48 +0800

    完成作业

--- 第四十二天到第四十三天：开一家餐厅吧（一）/example.js
+++ 第四十二天到第四十三天：开一家餐厅吧（一）/example.js
@@ -1,3 +1,5 @@
+
+
 // 餐厅类
 function restaurant(cash,seats,staff){
 	this.cash = cash;
@@ -31,6 +33,11 @@ function waiter(ID,name,salary){
 	memeber.call(this,ID,name,salary);
 }
 	//服务员
+var waiter = function(ID,name,salary) {
+    memeber.call(this,ID, name,salary);
+
+};
+
 waiter.prototype.waiterWork = function(par){
 	if(par.isArray()){
 		alert(par);
@@ -38,14 +45,34 @@ waiter.prototype.waiterWork = function(par){
 		alert("上菜");
 	}
 }
+
+waiter.getInstance = (function(ID,name,salary) {
+    var instance;
+    return function(ID,name,salary){
+        if (!instance) {
+            instance = new waiter(ID,name,salary);
+        }
+    }
+})();
+
+
 	//厨师
-function Cook(ID,name,salary){
+var Cook = function(ID,name,salary){
 	memeber.call(this,ID,name,salary);
 }
 
 Cook.prototype.cookWork = function(){
 	alert("做菜");
 }
+
+Cook.getInstance = (function(ID,name,salary){
+	var instance;
+	return function(ID,name,salary){
+		if(!instance){
+			instance = new Cook(ID,name,salary);
+		}
+	}
+})();
 
 //顾客
 function customer(){
@@ -78,5 +105,48 @@ var newCook = new Cook(1,"Tony", 10000);
 ifeRestaurant.hire(newCook);
 console.log(ifeRestaurant.staff);
 
-ifeRestaurant.fire(newCook);
-console.log(ifeRestaurant.staff);
+
+//这是一个顾客的队列
+var customers =  new Array('夏丽','黎明','倩倩');
+
+//菜单
+var menu = {
+	name:['水煮鱼','蔬菜撒拉','炒饭'],
+	price:['11元','12元','13元']
+}
+
+//点菜
+var orderMenu = function(){
+	var number = Math.ceil(Math.random()*2); 
+	console.log('顾客点了' + menu.name[number] + ' 价格是' + menu.price[number]);
+
+	return menu.name[number];
+}
+
+
+
+//一个启动程序
+var init = function(){
+	console.log('------餐馆即将开始运作-----');
+	for(var i=0;i<3;i++){
+		console.log(customers[i] + '进入了餐馆');
+		
+		var menuName = orderMenu();
+		console.log('服务员告诉厨师顾客点了' + menuName);
+		console.log('厨师正在做菜');
+		// for(var j=5;j>0;j--){
+		// 	console.log('倒计时 ' + j);
+		// }
+		console.log('服务员上菜');
+		console.log('顾客开始吃菜');
+		// for(var z=3;z>0;z++){
+		// 	console.log('倒计时 ' + z);
+		// }
+		console.log('顾客离开，下一位顾客即将进入');
+	}
+
+	console.log('今天餐馆营业完毕');
+}
+
+//执行营业函数
+init();
